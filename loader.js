12d8d17a53478dbde94a49ff02a6f52db92eaf74
Pankajapplication <pankaj.appsquadz@gmail.com>
2026-01-02 12:44:54 +0530

    add and update pages along features

---
+++ loader.js
@@ -0,0 +1,24 @@
+let percent = document.getElementById('circle');
+let circleBx = document.querySelector('.profile-bx');
+
+function progressBar(value){
+    circleBx.style.setProperty(
+        'background',
+        `conic-gradient(red ${percent.textContent}, #ddd 0%)`
+    );
+}
+
+function counter() {
+    let i = 0;
+
+    const interval = setInterval(() => {
+        percent.textContent = `${i}%`;
+        progressBar(i);
+        if (i >= 100) {
+            clearInterval(interval);
+        }
+        i++;
+    }, 30);
+}
+
+document.addEventListener('DOMContentLoaded', counter);
